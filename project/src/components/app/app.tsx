e4236dd249b3875233d3d625aa58515a63c637d2
B3rzxd3v <d3vilc0re@gmail.com>
2022-10-05 00:43:33 +0700

    Create MainScreen

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,5 +1,13 @@
-function App(): JSX.Element {
-  return <p>Hello, world!</p>;
+import MainScreen from '../../pages/main-screen/main-screen';
+
+type AppProps = {
+  name: string;
+  genre: string;
+  year: string;
+}
+
+function App({name, genre, year}: AppProps): JSX.Element {
+  return <MainScreen name='The Grand Budapest Hotel' year='2014' genre='Drama'/>;
 }
 
 export default App;
