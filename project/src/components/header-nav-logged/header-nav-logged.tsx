19c5f4e24ca9be40d116bcbdb480c90c0f8c27b7
B3rzxd3v <d3vilc0re@gmail.com>
2022-11-05 11:32:33 +0700

    TZ 1.1

--- project/src/components/header-nav-logged/header-nav-logged.tsx
+++ project/src/components/header-nav-logged/header-nav-logged.tsx
@@ -1,21 +1,27 @@
 import { MouseEvent } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
+import { AppRoute } from '../../const';
 import { useAppDiapatch, useAppSelector } from '../../hooks';
 import { logoutAction } from '../../services/api-actions';
 
 function HeaderNavLogged(): JSX.Element {
   const {userInfo} = useAppSelector((state) => state);
   const dispatch = useAppDiapatch();
+  const navigate = useNavigate();
 
   function clickHandler(evt: MouseEvent) {
     evt.preventDefault();
     dispatch(logoutAction());
   }
+
+  function avatarClickHandler() {
+    navigate(AppRoute.MyList);
+  }
   return (
     <ul className="user-block">
       <li className="user-block__item">
         <div className="user-block__avatar">
-          <img src={userInfo?.avatarUrl} alt="User avatar" width="63" height="63" />
+          <img src={userInfo?.avatarUrl} alt="User avatar" width="63" height="63" onClick={avatarClickHandler}/>
         </div>
       </li>
       <li className="user-block__item">
